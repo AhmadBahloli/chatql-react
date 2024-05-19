9517f929b664a5b6a75d0abcdd739f3cd82fbba2
Ahmed Bhlouli <ahmad.bahloli97@gmail.com>
2024-05-19 19:17:37 +0300

    Updated without the amplify add api command

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,30 @@
+export type AmplifyDependentResourcesAttributes = {
+  "analytics": {
+    "chatqlreact": {
+      "Id": "string",
+      "Region": "string",
+      "appName": "string"
+    }
+  },
+  "api": {
+    "chatqlreact": {
+      "AuthCognitoUserPoolIdOutput": "string",
+      "AuthCognitoUserPoolJSClientId": "string",
+      "AuthCognitoUserPoolNativeClientId": "string",
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "cognito5a6ec086": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "AppClientSecret": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
